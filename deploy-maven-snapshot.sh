51917119d79aa143dcebde59e4b09795e744405d
Christian Autermann <c.autermann@52north.org>
2019-09-18 13:44:44 +0200

    Update deploy-maven-snapshot.sh

--- deploy-maven-snapshot.sh
+++ deploy-maven-snapshot.sh
@@ -8,14 +8,14 @@ stop() {
 [ "${TRAVIS_BRANCH}" = "${SNAPSHOT_BRANCH}" ] \
   || stop "won't build branch ${TRAVIS_BRANCH}"
 
+[ "${TRAVIS_PULL_REQUEST}" = "false" ] \
+  || stop "no deployment for pull requests"
+
 [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] \
   || stop "no secure enviroment variables were provided"
 
 [ "${TRAVIS_JOB_NUMBER}" = "${TRAVIS_BUILD_NUMBER}.1" ] \
   || stop "only the first build job will be deployed"
-
-[ "${TRAVIS_EVENT_TYPE}" = "false" ] \
-  || stop "no deployment for cron jobs"
   
 [ "${DEPENDENT_BUILD}" != "true" ] \
   || stop "no deployment for dependent build"
